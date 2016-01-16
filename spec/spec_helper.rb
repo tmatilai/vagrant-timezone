0008a90e413fbed3d948816e7f5dfc73339ef70d
Teemu Matilainen <teemu.matilainen@iki.fi>
2016-01-16 14:57:56 +0200

    Remove unused Coveralls config from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,3 @@
-require 'simplecov'
-require 'coveralls'
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
-SimpleCov.start do
-  coverage_dir('tmp/coverage')
-  add_filter '/spec/'
-end
-
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
