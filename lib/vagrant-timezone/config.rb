fcd23c52f8e749d7f4f2c6c8c51fd9caba588ea4
Teemu Matilainen <teemu.matilainen@iki.fi>
2016-01-16 15:07:48 +0200

    Move `:host` handling to `finalize!`

--- lib/vagrant-timezone/config.rb
+++ lib/vagrant-timezone/config.rb
@@ -6,7 +6,7 @@ module VagrantPlugins
     #
     # @!parse class Config < Vagrant::Plugin::V2::Config; end
     class Config < Vagrant.plugin('2', :config)
-      attr_reader :value
+      attr_accessor :value
 
       def initialize
         super
@@ -14,8 +14,12 @@ module VagrantPlugins
         @value = UNSET_VALUE
       end
 
-      def value= value
-        if value == :host
+      def finalize!
+        super
+
+        if @value == UNSET_VALUE
+          @value = nil
+        elsif @value == :host
           # Get the offset of the current timezone of the host. Ruby doesn't reliably
           # detect the named timezone, so we have to use the hour offset. Note that when
           # DST changes, etc, this offset will change.
@@ -23,15 +27,7 @@ module VagrantPlugins
           # We set timezone offset negative to match POSIX standards
           # https://github.com/eggert/tz/blob/master/etcetera
           @value = sprintf('Etc/GMT%+d', -(Time.now.utc_offset/3600))
-        else
-          @value = value
         end
-      end
-
-      def finalize!
-        super
-
-        @value = nil if @value == UNSET_VALUE
       end
     end
   end
